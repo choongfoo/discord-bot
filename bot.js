2969713bb9f1507cb5d58d4fb1afda834635db58
choongfoo <choongfoo@users.noreply.github.com>
2018-01-21 18:16:03 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,18 +13,3 @@ client.on('message', message => {
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
-
-const { spawn } = require('child_process');
-const bat = spawn('runbot.bat']);
-
-bat.stdout.on('data', (data) => {
-  console.log(data.toString());
-});
-
-bat.stderr.on('data', (data) => {
-  console.log(data.toString());
-});
-
-bat.on('exit', (code) => {
-  console.log(`Child exited with code ${code}`);
-});
